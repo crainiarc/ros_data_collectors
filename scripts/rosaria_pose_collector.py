ab48c1e98497570f5d2a52380cedbe3f383c1b64
Keng Kiat Lim <kkin@crnrc.com>
2014-12-15 13:34:30 +0800

    Added rosaria_pose_collector.py

---
+++ scripts/rosaria_pose_collector.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import rospy
+from nav_msgs.msg import Odometry
+from data_collector import DataCollector
+import rosbridge_library
+
+class RosAriaPoseCollector(DataCollector):
+
+    def __init__(self):
+        super(RosAriaPoseCollector, self).__init__()
+        self.data_topic = rospy.get_param('~data_topic', 'RosAria/pose')
+
+        rospy.Subscriber(self.data_topic, Odometry, self.data_callback)
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    rospy.init_node('rosaria_pose_collector', anonymous=True)
+    node = RosAriaPoseCollector()
