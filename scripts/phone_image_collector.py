330ed7958c8e5732d7de17b19b90bcd51c5cb9f4
Keng Kiat Lim <kkin@crnrc.com>
2014-12-15 14:11:51 +0800

    Added more functionality to phone_image_collector.py

--- scripts/phone_image_collector.py
+++ scripts/phone_image_collector.py
@@ -8,17 +8,43 @@ from data_collector import DataCollector
 class PhoneImageCollector(DataCollector):
 
     def __init__(self):
-        super(RosAriaPoseCollector, self).__init__()
+        super(PhoneImageCollector, self).__init__()
 
         self.socket_port = rospy.get_param('~port', '50000')
-        self.socket_host = rospy.get_param('~host', '127.0.0.1')
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_sock.bind((socket.gethostname(), int(self.socket_port)))
+        self.server_sock.listen(1)
+        rospy.loginfo("Start listening for connections on %s:%s", socket.gethostname(), self.socket_port)
+
+        self.client_sock, address = self.server_sock.accept()
+        rospy.loginfo("Client connected from  %s", address)
 
         rospy.spin()
 
 
-    def _connect_to_phone(self):
-        self.sock.connect((self.socket_host, self.socket_port))
+    def _joystick_triggered(self, joystick):
+        if joystick.buttons[self.trigger_button] == 1:
+            self.id += 1
+
+            self._request_data()
+            data_buffer = self._receive_data()
+
+            self.data_list += [{'id': self.id, 'data': extract_values(data)}]
+            self._write_to_file()
+
+
+    def _request_data(self):
+      msg = 1
+      total_sent = 0
+      while total_sent < 1:
+          sent = self.client_sock.send(msg)
+          if sent == 0:
+              raise RuntimeError("socket connection broken")
+          total_sent = total_sent + sent
+
+
+    def _receive_data(self):
+      pass
 
 
 if __name__ == "__main__":
