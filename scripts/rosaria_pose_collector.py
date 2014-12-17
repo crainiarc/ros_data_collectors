49ab00ec4f4310fbefda990be7abc1b81289369f
Keng Kiat Lim <kkin@crnrc.com>
2014-12-17 16:11:13 +0800

    Removed unnecessary imports in rosaria_pose_collector.py

--- scripts/rosaria_pose_collector.py
+++ scripts/rosaria_pose_collector.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 import rospy
 from nav_msgs.msg import Odometry
 from data_collector import DataCollector
-import rosbridge_library
 
 class RosAriaPoseCollector(DataCollector):
 
