f4827aeabbde71fd74acde356cccad6e264c6282
Keng Kiat Lim <kkin@crnrc.com>
2014-12-30 18:49:00 +0800

    Added an adapter to automatically trigger the joystick from rosbags

---
+++ scripts/joystick_trigger.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import rospy
+from sensor_msgs.msg import Joy
+
+module_name = None
+message_type = None
+trigger_button = None
+
+
+def callback(data):
+    pub = rospy.Publisher('joy', Joy)
+    joy_msg = Joy()
+    joy_msg.buttons[trigger_button] = 1
+    pub.publish(joy_msg)
+
+
+def joy_relay():
+    global module_name, message_type, trigger_button
+    rospy.init_node('joy_relay', anonymous=True)
+
+    module_name = rospy.get_param('~module_name', 'std_msgs.msg')
+    message_type = rospy.get_param('~message_type', 'String')
+    message_topic = rospy.get_param('~message_topic', 'joy')
+    trigger_button = int(rospy.get_param('~trigger_button', '0'))
+
+    message_class = getattr((__import__(module_name)).msg, message_type)
+    rospy.Subscriber(message_topic, message_class, callback)
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    joy_relay()
